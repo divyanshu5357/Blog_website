05ea2d0b286ff52d6822c9766cdd03919c38ce2d
divyanshu5357 <divyanshusingh17102002@gmail.com>
2026-08-21 17:03:28 +0530

    fix(admin): resolve mousepad scrolling issue by disabling lenis smooth scroll on admin routes

--- client/src/components/SmoothScroll.jsx
+++ client/src/components/SmoothScroll.jsx
@@ -1,8 +1,16 @@
 import { useEffect } from "react";
 import Lenis from "lenis";
+import { useLocation } from "react-router-dom";
 
 export default function SmoothScroll() {
+  const location = useLocation();
+
   useEffect(() => {
+    // Disable smooth scroll on admin routes to prevent scroll hijacking
+    if (location.pathname.startsWith('/admin')) {
+      return;
+    }
+
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
@@ -10,17 +18,20 @@ export default function SmoothScroll() {
       touchMultiplier: 1.5,
     });
 
+    let animationFrameId;
+
     function raf(time) {
       lenis.raf(time);
-      requestAnimationFrame(raf);
+      animationFrameId = requestAnimationFrame(raf);
     }
 
-    requestAnimationFrame(raf);
+    animationFrameId = requestAnimationFrame(raf);
 
     return () => {
+      cancelAnimationFrame(animationFrameId);
       lenis.destroy();
     };
-  }, []);
+  }, [location.pathname]);
 
   return null;
 }
